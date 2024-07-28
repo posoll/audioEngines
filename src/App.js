64af4be13fe681632f8bdbdf7c0d2adc146aa478
posoll <posoll@protonmail.com>
2024-07-28 23:18:21 +0300

    add new util

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "../node_modules/bootstrap/dist/js/bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Todo from './Components/Todo';
+
+function App() {
+  return (
+    <div className='container'>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
